7aa3befa8c88540bc5672086bb413018d089643a
Silvanos Eric <ericoderito@gmail.com>
2024-07-16 13:29:12 +0300

    feat: fetch and display toy objects from API
    - Add fetch request to retrieve all toy objects from the API endpoint at
      `http://localhost:3000/toys`.
    - Create and append toy cards to the DOM dynamically using a document fragment to improve
      performance.
    - Each toy card includes:
      - Toy name (h2 element)
      - Toy image (img element with class "toy-avatar")
      - Toy likes count (p element)
      - Like button (button element with class "like-btn" and toy ID)
    
    This enhancement populates the toy collection section with data fetched from the server, providing a
    dynamic and interactive user experience.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,33 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  // Make fetch request to get all the toy objects
+  const toyCollectionEl = document.querySelector("div#toy-collection");
+  const cardsFragment = document.createDocumentFragment();
+
+  fetch("http://localhost:3000/toys")
+    .then((response) => response.json())
+    .then((data) => {
+      for (toy of data) {
+        const cardEl = document.createElement("div");
+        cardEl.classList.add("card");
+
+        const h2 = document.createElement("h2");
+        h2.textContent = toy.name;
+        const img = document.createElement("img");
+        img.setAttribute("src", toy.image);
+        img.classList.add("toy-avatar");
+        const p = document.createElement("p");
+        p.textContent = toy.likes;
+        const button = document.createElement("button");
+        button.textContent = "Like ❤️";
+        button.classList.add("like-btn");
+        button.setAttribute("id", toy.id);
+
+        cardEl.append(h2, img, p, button);
+        cardsFragment.appendChild(cardEl);
+      }
+    })
+    .then(() => toyCollectionEl.appendChild(cardsFragment));
 });
