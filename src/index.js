e6c8fbefb3410f6bffe22d8af9e533c61f27a990
Silvanos Eric <ericoderito@gmail.com>
2024-07-16 15:13:17 +0300

    feat(toys): add dynamic toy fetch and creation functionality
    - Change toy IDs in `db.json` from numbers to strings.
    - Add new toy objects to `db.json`: Jessie, Sid Phillips, and Mrs. Potato Head.
    - Update `index.html` to make toy name and image URL input fields required.
    - Reformat `index.html` for consistency.
    
    - Refactor `index.js` to:
      - Cache `toyCollectionEl` and `toyCollectionFragment` elements.
      - Fetch and display toys from the API using `createToyElements` and `createToyElement` utility
        functions.
      - Add form submission functionality to create new toys dynamically and append them to the toy
        collection.
    
    This update improves the application by enabling dynamic fetching and creation of toy objects,
    enhancing user interaction and data management.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 let addToy = false;
 
+// Elements of interest
+const toyCollectionEl = document.querySelector("div#toy-collection");
+const toyCollectionFragment = document.createDocumentFragment();
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -14,31 +18,66 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // Make fetch request to get all the toy objects
-  const toyCollectionEl = document.querySelector("div#toy-collection");
-  const cardsFragment = document.createDocumentFragment();
 
   fetch("http://localhost:3000/toys")
     .then((response) => response.json())
-    .then((data) => {
-      for (toy of data) {
-        const cardEl = document.createElement("div");
-        cardEl.classList.add("card");
+    .then(createToyElements)
+    .then(() => toyCollectionEl.appendChild(toyCollectionFragment));
 
-        const h2 = document.createElement("h2");
-        h2.textContent = toy.name;
-        const img = document.createElement("img");
-        img.setAttribute("src", toy.image);
-        img.classList.add("toy-avatar");
-        const p = document.createElement("p");
-        p.textContent = toy.likes;
-        const button = document.createElement("button");
-        button.textContent = "Like ❤️";
-        button.classList.add("like-btn");
-        button.setAttribute("id", toy.id);
+  // Functionality to add a new toy
+  const form = document.querySelector("form.add-toy-form");
 
-        cardEl.append(h2, img, p, button);
-        cardsFragment.appendChild(cardEl);
-      }
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const toyName = form.querySelector("input[name='name']");
+    const toyImg = form.querySelector("input[name='image']");
+
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: toyName.value,
+        image: toyImg.value,
+        likes: 0,
+      }),
     })
-    .then(() => toyCollectionEl.appendChild(cardsFragment));
+      .then((response) => response.json())
+      .then(createToyElement)
+      .then(() => toyCollectionEl.appendChild(toyCollectionFragment))
+      .then(() => {
+        toyName.value = "";
+        toyImg.value = "";
+      });
+  });
 });
+
+// Utility functions
+function createToyElements(toys) {
+  for (toy of toys) {
+    createToyElement(toy);
+  }
+}
+
+function createToyElement(toy) {
+  const cardEl = document.createElement("div");
+  cardEl.classList.add("card");
+
+  const h2 = document.createElement("h2");
+  h2.textContent = toy.name;
+  const img = document.createElement("img");
+  img.setAttribute("src", toy.image);
+  img.classList.add("toy-avatar");
+  const p = document.createElement("p");
+  p.textContent = toy.likes;
+  const button = document.createElement("button");
+  button.textContent = "Like ❤️";
+  button.classList.add("like-btn");
+  button.setAttribute("id", toy.id);
+
+  cardEl.append(h2, img, p, button);
+  toyCollectionFragment.appendChild(cardEl);
+}
